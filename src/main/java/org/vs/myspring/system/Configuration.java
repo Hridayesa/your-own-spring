c78faffc05134bb67420f481c437dc40ab4e01ae
Hridayesa <denis.karpoff@gmail.com>
2018-07-05 15:59:55 +0300

    solution

--- src/main/java/org/vs/myspring/system/Configuration.java
+++ src/main/java/org/vs/myspring/system/Configuration.java
@@ -1,19 +1,44 @@
 package org.vs.myspring.system;
 
+import org.reflections.Reflections;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Denis Karpov on 05.07.2018.
  */
 public class Configuration {
     private final Map<Class, Class> implClasses = new HashMap<>();
+    private final Reflections scanner;
 
-    public Configuration() {
+    public Configuration(String packageName) {
+        scanner = new Reflections(packageName);
     }
 
-    public <T> Class<T> getImplClass(Class<T> key) {
-        Class<T> res = (Class<T>)implClasses.get(key);
-        return (res != null) ? res : key;
+    public <T> Class<? extends T> getImplClass(Class<T> key) {
+        // пробуем взять из кэша
+        Class<? extends T> res = (Class<? extends T>)implClasses.get(key);
+
+        // сканируем
+        if (res==null){
+            res = scanForImpl(key);
+        }
+        res = (res != null) ? res : key;
+
+        // кэшируем
+        if (res!=null){
+            implClasses.put(key, res);
+        }
+        return res;
+    }
+
+    private <T> Class<? extends T> scanForImpl(Class<T> key) {
+        Set<Class<? extends T>> set = scanner.getSubTypesOf(key);
+        if (set.size() != 1) {
+            throw new RuntimeException(key + " has 0 or more than one impl.");
+        }
+        return set.iterator().next();
     }
 }
--- src/main/java/org/vs/myspring/system/MyApplicationContext.java
+++ src/main/java/org/vs/myspring/system/MyApplicationContext.java
@@ -4,7 +4,7 @@ import lombok.SneakyThrows;
 
 public class MyApplicationContext {
     private final static MyApplicationContext context = new MyApplicationContext();
-    private Configuration configuration = new Configuration();
+    private Configuration configuration = new Configuration("org.vs.myspring"); // Название пакета потом надо будет убрать
 
     public static MyApplicationContext getInstance(){
         return context;
@@ -12,7 +12,7 @@ public class MyApplicationContext {
 
     @SneakyThrows
     public <T> T getBean(Class<T> key){
-        Class<T> implClass = configuration.getImplClass(key);
+        Class<? extends T> implClass = configuration.getImplClass(key);
 
         T res = (T)implClass.newInstance();
 
